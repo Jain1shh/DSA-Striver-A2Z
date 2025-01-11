54847228263dcc8e63753e9764876c6bd1e9cfaf
Jainish Prajapati <jainishprajapati98@gmail.com>
2025-01-11 19:44:25 +0530

    a pattern and directory & file for stl

---
+++ 1) patterns/7.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n-i-1; j++){
+            cout << " ";
+        }
+
+        for(int j = 0; j < 2*i+1; j++){
+            cout << "*";
+        }
+
+        for(int j = 0; j < n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Input rows: "; cin >> n;
+    cout << endl;
+    print(n);
+    return 0;
+}
