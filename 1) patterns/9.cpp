0586702badf90454580470cc2515c682fb5f543a
Jainish Prajapati <jainishprajapati98@gmail.com>
2025-01-12 14:34:54 +0530

    some patterns

---
+++ 1) patterns/9.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n-i-1; j++){
+            cout << " ";
+        }
+
+        for(int j = 0; j < 2*i+1; j++){
+            cout << "*";
+        }
+
+        for(int j = 0; j < n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        
+        for(int j = 0; j < 2*n - (2*i + 1); j++){
+            cout << "*";
+        }
+
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Input rows: "; cin >> n;
+    cout << endl;
+    print(n);
+    return 0;
+}
