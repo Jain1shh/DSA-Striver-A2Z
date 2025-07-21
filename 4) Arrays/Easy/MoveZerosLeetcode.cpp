c17150091902b84d4faeba63764ac63832bde739
Jain1shh <jainishprajapati98@gmail.com>
2025-07-21 12:51:45 +0530

    Move Zeros leetcode solution

---
+++ 4) Arrays/Easy/MoveZerosLeetcode.cpp
@@ -0,0 +1,18 @@
+// Only leetcode solution without main()
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int insertPos = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[insertPos++] = nums[i];
+            }
+        }
+
+        while (insertPos < nums.size()) {
+            nums[insertPos++] = 0;
+        }
+    }
+};
